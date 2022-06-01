1493e200bde028ab0e71a7b211fab672783a82a6
wangechi <rahabsheshi22@gmail.com>
2022-06-01 23:26:51 +0000

    combined

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    string = uppercase
+    for i in str:
+        if(ord(i)>=65 and ord(i)<=90):
+            print('{}'.format(string))
+            print('{}'.format(isupper()))
