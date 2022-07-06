0dc8104cf48d179a2923bf7e72494739d66eb785
WangechiRay <rahabsheshi22@gmail.com>
2022-07-06 06:47:35 +0000

    append file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines append_write function"""
+
+def append_write(filename="", text=""):
+    """appends a string at the end of a textfile(utf-8)"""
+    with open(filename, encoding="utf-8")as f:
+        return f.append(text)
+
