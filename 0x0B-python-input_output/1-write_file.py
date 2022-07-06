0c3b96783344dd0faae2a2e849ffb427319a88c2
WangechiRay <rahabsheshi22@gmail.com>
2022-07-06 06:29:45 +0000

    files

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,7 +4,7 @@ Contains the number_of_lines function
 """
 
 
-def  write_file(filename="", text=""):
+def write_file(filename="", text=""):
     """returns the number of chars written to "filename" from "text" """
     with open(filename, 'w', encoding='utf=8') as f:
         return f.write(text)
