5eb1c59d46d6d9cdc7e0edaa647df7f98baf9a13
WangechiRay <rahabsheshi22@gmail.com>
2022-07-06 19:41:37 +0000

    json string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-import json
-
 """
 Write a function that returns the JSON representation of an object (string)
 """
 
 
+import json
 def to_json_string(my_obj):
     """the function returns the JSON representation of an object"""
     return json.dumps(my_obj)
